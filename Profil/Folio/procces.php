df224e4ae690f72baf469dd6a7a7273ea28b9b1f
faisalazkha <faisalazkha@gmail.com>
2019-01-03 14:04:24 +0700

    First website upload

---
+++ Profil/Folio/procces.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Keluaran Form</title>
+	</head>
+	<body align="center">
+		<h3>Data Penfdaftaran</h3>
+		<?php
+			echo "Nama :".$_POST['nama']."</br>";
+			echo "Password :".$_POST['Password']."</br>";
+			echo "Jenis Kelamin :".$_POST['Jenis Kelamin']."</br>";
+			echo "Keahlian :".$_POST['Keahlian']."</br>";
+			echo "Tempat Lahir :".$_POST['Tempat Lahir']."</br>";
+			echo "Deskripsi Diri :".$_POST['Deskripsi Diri']."</br>";
+		?>
+	</body>
+</html>
